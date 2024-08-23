a760642958b5a9ab6dd714cd4dc7b9b3f3a044b1
LuccasAps <luccasasahe@gmail.com>
2024-08-23 15:13:23 -0300

    exercicio 7

--- exercicio7.c
+++ exercicio7.c
@@ -2,15 +2,13 @@
 
 int main(){
     int inteiro, endereco;
-    endereco = &inteiro;
     int *pi = &inteiro;
-    if (endereco % 5 == 0)
+    if (pi % 5 == 0)
     {
         printf("O endereco de memoria eh divisivel por 5");
     }
     else{
         printf("O endereco de nao memoria eh divisivel por 5");
     }
-
     return 0;
 }
