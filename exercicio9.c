b233e7ef7f431da25e118372203b2d7c118522b7
LuccasAps <luccasasahe@gmail.com>
2024-08-25 23:30:16 -0300

    exercicio9

--- exercicio9.c
+++ exercicio9.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
+
     int a, b, c;
     int *pA = &a;
     int *pB = &b;
+    scanf("%d %d %d", &a, &b ,&c);
+    if (*pA > *pB)
+    {
+       *pB = c;
+       if (*pA > *pB)
+       {
+        printf("o maior valor é %d",*pA);
+        return 0;
+       }
+
+    }else
+    {
+        *pA = c;
+        printf("%d", *pA);
+        if (*pB > *pA)
+        {
+            printf("o maior valor é %d",*pB);
+            return 0;
+        }else{
+            printf("o maior valor é %d",*pA);
+            return 0;
+        }
+    }
+    return 0;
 }
