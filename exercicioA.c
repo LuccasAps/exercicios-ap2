615c67513f9ff169cf4659b78c884e04d1614001
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2024-10-15 20:14:47 -0300

    Update exercicioA.c

--- exercicioA.c
+++ exercicioA.c
@@ -6,6 +6,7 @@
 typedef struct{
     char nome[10];
     float n1,n2,n3;
+    float media
     int freq;
 }Aluno;
 
@@ -14,8 +15,14 @@ float calcmedia(float *n1,float *n2,float *n3){
     media = (*n1+*n2+*n3)/3;
     return media;
 }
-void imprime(char nome[10], float n1, float n2, float n3, int freq){
+void imprime(char nome[10], float media, int freq){
     printf("%s\n%f %f %f\n%d",nome, n1, n2, n3, freq);
+    if(freq >= 75){
+        printf("Aprovado");
+    }
+    else{
+        printf("Reprovado");
+    }
 }
 
 int main(){
@@ -25,7 +32,10 @@ int main(){
     int i;
     for(i=0;i<MAX;i++){
         scanf("%s",aluno[i].nome);
-        scanf("%f %f %f",aluno[i].n1, aluno[i].n2, aluno[i].n2);
-        scanf("%d",aluno[i].freq);
+        scanf("%f %f %f",&aluno[i].n1, &aluno[i].n2, &aluno[i].n3);
+        aluno[i].media = calcmedia(aluno[i].n1 + aluno.n2 + aluno.n3)/3;
+        scanf("%d",&aluno[i].freq);
     }
+    for(i=0;i<MAX;i++)
+        imprime(aluno[i].nome, aluno[i].media, aluno[i].freq);
 }
